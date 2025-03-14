45eca02fb8dadc31945865275aa28a8e97939ac1
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-03-14 14:53:26 +0200

    Project Maintenance

--- src/components/BookCatalogue.vue
+++ src/components/BookCatalogue.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-    <p>Book Catalogue.</p>
+    <h1>Book Catalogue.</h1>
   </div>
 </template>
 
 <script>
+import bookService from '../services/bookService';
+
 export default {
-  name: 'BookCatalogue'
+  name: 'BookCatalogue',
+  data() {
+    return {
+      books: [],
+      loading: true
+    }
+  },
+  mounted() {
+    this.fetchBooks();
+  },
+  methods: {
+    fetchBooks() {
+      bookService.getAllBooks()
+        .then(response => {
+          this.books = response.data;
+          this.loading = false;
+        })
+        .catch(error => {
+          console.error('Error whilst fetching books.', error);
+          this.loading = false;
+        });
+    }
+  }
 }
 </script>
 
