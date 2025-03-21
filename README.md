adb82ad03b2849e6b11267d38748cfc2c6e9022d
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-03-21 11:12:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Book Catalogue Front-end VueJs App
+
+- Serves as the client side for the Book Catalogue Web Application.
+- Connects to the back-end Spring boot application (https://github.com/KimarenNaidoo/BookCatalogueBackEndApp)
+
---
+++ src/components/BookCatalogue.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Book Catalogue.</h1>
+    <div v-if="loading">Loading books...</div>
+    <div v-else>
+      <div v-for="book in books" :key="book.id" class="book-item">
+        <h3>Title: {{ book.title }}</h3>
+        <p>Author/s: {{ book.author }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import bookService from '../services/bookService';
+
+export default {
+  name: 'BookCatalogue',
+  data() {
+    return {
+      books: [],
+      loading: true
+    }
+  },
+  mounted() {
+    this.fetchBooks();
+  },
+  methods: {
+    fetchBooks() {
+      bookService.getAllBooks()
+        .then(response => {
+          this.books = response.data;
+          this.loading = false;
+        })
+        .catch(error => {
+          console.error('Error whilst fetching books.', error);
+          this.loading = false;
+        });
+    }
+  }
+}
+</script>
+
