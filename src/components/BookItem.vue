51222bb9dff255aef31f88acd0f5fa4a84b9baea
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-04-04 12:40:05 +0200

    fix: Update view to include the updated book dto

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="book-item">
-        <h3><strong>Book Id:</strong> {{ book.id }}</h3>
-        <p><strong>Title:</strong> {{ book.title }}</p>
-        <p><strong>Status:</strong> {{ book.statusId }}</p>
-        <p><strong>Category:</strong> {{ book.categoryId }}</p>
+        <h3><strong>Title:</strong> {{ book.title }}</h3>
+        <p><strong>Status:</strong> {{ book.status }}</p>
+        <p><strong>Category:</strong> {{ book.category }}</p>
         <p><strong>Author:</strong> {{ book.author }}</p>
-        <p><strong>Ownership:</strong> {{ book.ownershipId }}</p>
+        <p><strong>Ownership:</strong> {{ book.ownership }}</p>
         <p><strong>ISBN:</strong> {{ book.isbn }}</p>
-        <p><strong>Create Date:</strong> {{ book.createDate }}</p>
     </div>
 </template>
 
