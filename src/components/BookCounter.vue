f12211837d684bf1d910a522faf5465c61303ee7
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-10-06 19:37:55 +0200

    Creating a different component for the page counter, next and prev. buttons

---
+++ src/components/BookCounter.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div v-if="currentOffset != 0" id="prev-button">
+            <button @click="fetchPrevBooks">Previous</button>
+        </div>
+        <div id="page-indicator">
+            {{ page }}
+        </div>
+        <div v-if="(currentOffset + limit) < totalBooks" id ="next-button">
+            <button @click="fetchNextBooks">Next</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BookCounter',
+    props: {
+        totalBooks: {
+            type: Number,
+            required: true
+        },
+        currentOffset: {
+            type: Number,
+            required: true
+        },
+        limit: {
+            type: Number,
+            required: true
+        }
+    },
+    data() {
+        return {
+            page: 1
+        }
+    },
+    methods: {
+        fetchNextBooks() {
+            this.page++;
+            this.$emit('next-data');
+        },
+        fetchPrevBooks() {
+            this.page--;
+            this.$emit('prev-data');
+        }
+    }
+</script>
